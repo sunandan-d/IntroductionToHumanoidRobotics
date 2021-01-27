03d9c4d939ee706435b089abd2ec88203a2bb958
Sunandan Dutta <40659257+sunandan-d@users.noreply.github.com>
2021-01-27 19:23:43 +0900

    Update fk_random.m

--- fk_random.m
+++ fk_random.m
@@ -14,7 +14,7 @@ rand('state',0);  % A random number is generated at the initial state
 
 figure
 while 1
-    qR1 = 2/3*pi*(rand(6,1)-0.5);  %  -pi/2 < q < pi/2
+    qR1 = 2/3*pi*(rand(6,1)-0.5);  %  -pi/2 < q < pi/2.....Generating a 6-by-1 column vector of random numbers
     qR1(4) = pi*rand;          %   0 < q4 < pi 
     
     qL1 = pi*(rand(6,1)-0.5);  %  -pi/2 < q < pi/2
