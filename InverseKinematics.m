4b2699e44b188c70f9614fe6f703559740dd245f
Sunandan Dutta <40659257+sunandan-d@users.noreply.github.com>
2021-01-28 19:14:32 +0900

    Update InverseKinematics.m

--- InverseKinematics.m
+++ InverseKinematics.m
@@ -2,13 +2,13 @@ function err_norm = InverseKinematics(to, Target)
 global uLINK
 
 lambda = 0.9;
-idx = FindRoute(to);
+idx = FindRoute(to);                  % Find out a route for the body to the target link
 ForwardKinematics(1);
-err = CalcVWerr(Target, uLINK(to));
+err = CalcVWerr(Target, uLINK(to));   % Calculate the error in the position and attitude of the link
 for n = 1:10
   if norm(err) < 1E-6 break, end;
-  J  = CalcJacobian(idx);
-  dq = lambda * (J \ err);
+  J  = CalcJacobian(idx);             % Funtion to Calculate the Jacobian
+  dq = lambda * (J \ err);            % Solves the Linear Equation (Eq. 2.71) without doing the explicit matrix inversion
   MoveJoints(idx, dq);
   ForwardKinematics(1);
   err = CalcVWerr(Target, uLINK(to));
